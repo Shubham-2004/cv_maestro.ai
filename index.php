54f2c190e70b0eab5888a7de68a1147f719e38cf
Shubham-2004 <shubhambera100@gmail.com>
2024-10-02 23:48:52 +0530

    adding homepage with the style.css

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shiksha Ai</title>
+    <link rel = "stylesheet" type = "text/css" href = "styles.css">
+</head>
+<body class ="started_body">
+    <div class="started_container">
+        <h1 class = "started_text"> CV Maestro.AI </h1>
+        <button class="started_button"><a> Get Started </a></button>
+    </div>
+</body>
+</html>
+
